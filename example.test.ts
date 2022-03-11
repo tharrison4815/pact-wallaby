f19e905db389141ef3b2bf3a6db6ddcc210bcbd7
Tom Harrison <tharrison@dunelm.com>
2022-03-11 16:08:02 +0000

    Initial commit

---
+++ example.test.ts
@@ -0,0 +1,34 @@
+import axios from "axios";
+import { Pact } from "@pact-foundation/pact";
+
+const pact = new Pact({
+  consumer: "mockConsumer",
+  provider: "mockProvider",
+  port: 1234,
+});
+
+beforeAll(async () => {
+  await pact.setup();
+
+  await pact.addInteraction({
+    state: "mockstate",
+    uponReceiving: "something",
+    withRequest: {
+      method: "GET",
+      path: "/mockpath",
+    },
+    willRespondWith: {
+      status: 200,
+      body: "mockBody",
+    },
+  });
+});
+
+it("should return mockBody", async () => {
+  const result = await axios.get("http://localhost:1234/mockpath");
+  expect(result.data).toBe("mockBody");
+});
+
+afterEach(() => pact.verify());
+
+afterAll(() => pact.finalize());
